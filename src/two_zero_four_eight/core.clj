7c27f3c2c0a3069cdd254f5331ff3afc7f3ae843
gayatrijagtap <gdjagtap1999@gmail.com>
2020-02-08 18:44:37 +0530

    Implement move-up and move-down.

--- src/two_zero_four_eight/core.clj
+++ src/two_zero_four_eight/core.clj
@@ -2,6 +2,8 @@
 
 (defn not-zero? [e] (not (zero? e)))
 
+(def transpose (partial apply mapv vector))
+
 (defn append-zeros [row]
   (concat row (repeat (- 4 (count row)) 0)))
 
@@ -11,13 +13,19 @@
 
 (defn evaluate [row]
   (->> row
+       (filter not-zero?)
        partition-identical-pair
-       (map (partial reduce +))
-       (filter not-zero?)))
+       (map (partial reduce +))))
 
 (defn move-left [grid]
   (map (comp append-zeros evaluate) grid))
 
 (defn move-right [grid]
   (map (comp reverse append-zeros evaluate reverse) grid))
+
+(defn move-up [grid]
+  (transpose (move-left (transpose grid))))
+
+(defn move-down [grid]
+  (transpose (move-right (transpose grid))))
 
