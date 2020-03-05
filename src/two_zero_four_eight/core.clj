716ca3e71818eee8d2a29233e0be8bb9470f25b1
gayatrijagtap <gayatrijagtap@Gayatris-MacBook-Pro.local>
2020-03-05 17:20:21 +0530

    implement start-game

--- src/two_zero_four_eight/core.clj
+++ src/two_zero_four_eight/core.clj
@@ -29,17 +29,52 @@
 (defn move-down [grid]
   (transpose (move-right (transpose grid))))
 
-(defn get-initial-grid
-  (mapv (partial repeat 4) (repeat 4 0)))
-
 (defn get-zero-indexes [coll]
-  (keep-indexed (fn [x y] (when (zero? y) x)) coll))
+      (keep-indexed (fn [x y] (when (zero? y) x)) coll))
 
-(defn insert-random-twos [grid n]
 
-  )
+(defn flat-grid [grid]
+      (vec (flatten grid)))
 
-(defn start-game
-  (loop [grid (get-initial-grid)]
-    )
-  )
+
+(defn add-random-twos [grid]
+      (let [flatten-grid (flat-grid grid) zero-indexes (get-zero-indexes flatten-grid)]
+           (if (empty? zero-indexes) -1 (assoc flatten-grid (rand-nth zero-indexes) 2))))
+
+
+(def initial-grid
+  (nth (iterate add-random-twos (mapv (partial repeat 4) (repeat 4 0))) 2))
+
+
+(defn print-grid
+      [grid]
+      (->> grid
+           (partition 4)
+           (map (fn [x] (clojure.string/join "|" x)))
+           (clojure.string/join "\n")
+           (println)
+           ))
+
+
+(defn update-grid [grid instruction] (case instruction
+                             "l" (move-left grid)
+                             "r" (move-right grid)
+                             "u" (move-up grid)
+                             "d" (move-down grid)
+                             (move-left grid)))
+
+
+(defn start-game []
+      (loop [grid initial-grid]
+            (print-grid grid)
+            (println "Please enter move (up:u, down:d, left:l, right:r):")
+            (let [instruction (read-line) updated-grid (flat-grid (update-grid (partition 4 grid) instruction))]
+                 (print-grid updated-grid)
+                 (println "-------------------")
+                 (if (empty? (get-zero-indexes updated-grid))
+                   (print "Game over!!")
+                   (recur (add-random-twos updated-grid))
+                   )
+                 )
+            )
+      )
